5d5bc04dc99afa328a7abf371c71e972952d2dda
u1973227 <100150954+u1973227@users.noreply.github.com>
2022-06-05 11:28:25 +0200

    Pause + resume + game over

--- prac_final/javascript/SceneMain.js
+++ prac_final/javascript/SceneMain.js
@@ -1,3 +1,51 @@
+var isPaused = false;
+var isGameOver = false;
+
+function runGame(){
+    if (condicio_mort){
+        clearInterval(interval);
+        gameOver();
+        return;
+    }
+}
+
+document.addEventListener('keyup', function(e)
+{
+    if(e.which == 32 && isGameOver == false){
+        if(isPaused) resumeGame();
+        else pauseGame()
+    }
+});
+
+function pauseGame(){
+    clearInterval(interval);
+    isPaused = true;
+    canvas.style.opacity = 0.5;
+    canvasContext.font = "90px tahoma";
+    canvasContext.fillStyle = "white";
+    canvasContext.textAlign = "center";
+    canvasContext.textBaseline = "middle";
+    canvasContext.fillText("Game Paused", 400, 250);
+}
+
+function resumeGame(){
+    isPaused = false;
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.style.opacity = 1;
+    interval = setInterval(runGame, 20);
+}
+
+function gameOver(){
+    isGameOver = true;
+    canvas.style.opacity = 0.5;
+    canvasContext.font = "90px tahoma";
+    canvasContext.fillStyle = "white";
+    canvasContext.textAlign = "center";
+    canvasContext.textBaseline = "middle";
+    canvasContext.fillText("Game Over", 400, 170);
+    canvasContext.fillText("You Scored" + score, 400, 330);
+}
+
 class SceneMain extends Phaser.Scene {
     constructor() {
         super('SceneMain');
